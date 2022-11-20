9917d903ebdf7642411e3cdc3d9d8f63583009a7
jowend <joaquinvcestudio@gmail.com>
2022-11-20 17:21:32 -0300

    Mocks con Go

--- curso-go-unit-testing-base-initial-project/controller/poke_api_handler.go
+++ curso-go-unit-testing-base-initial-project/controller/poke_api_handler.go
@@ -2,13 +2,20 @@ package controller
 
 import (
 	"catching-pokemons/models"
+	"catching-pokemons/util"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+)
+
+var (
+	ErrPokemonNotFound = errors.New("pokemon not found")
+	ErrPokeApiFailure  = errors.New("unexpected response in PokeApi")
 )
 
 // respondwithJSON write json response format
@@ -30,29 +37,48 @@ func respondwithJSON(w http.ResponseWriter, code int, payload interface{}) {
 func GetPokemon(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
 
+	apiPokemon, err := GetPokemonFromPokeApi(id)
+
+	if errors.Is(err, ErrPokemonNotFound) {
+		respondwithJSON(w, http.StatusNotFound, fmt.Sprintf("pokemon not found: %s", id))
+	}
+	if err != nil {
+		respondwithJSON(w, http.StatusInternalServerError, fmt.Sprintf("Error while calling pokeapi: %s", err.Error()))
+	}
+	parsedPokemon, err := util.ParsePokemon(apiPokemon)
+	if err != nil {
+		respondwithJSON(w, http.StatusInternalServerError, fmt.Sprintf("error found: %s", err.Error()))
+	}
+
+	respondwithJSON(w, http.StatusOK, parsedPokemon)
+}
+
+func GetPokemonFromPokeApi(id string) (models.PokeApiPokemonResponse, error) {
 	request := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%s", id)
 
 	response, err := http.Get(request)
 	if err != nil {
-		log.Fatal(err)
+		return models.PokeApiPokemonResponse{}, err
+	}
+
+	if response.StatusCode == http.StatusNotFound {
+		return models.PokeApiPokemonResponse{}, ErrPokemonNotFound
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return models.PokeApiPokemonResponse{}, ErrPokeApiFailure
 	}
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal(err)
+		return models.PokeApiPokemonResponse{}, err
 	}
 
 	var apiPokemon models.PokeApiPokemonResponse
 
 	err = json.Unmarshal(body, &apiPokemon)
 	if err != nil {
-		log.Fatal(err)
+		return models.PokeApiPokemonResponse{}, err
 	}
-	/*
-		parsedPokemon, err := util.ParsePokemon(apiPokemon)
-		if err != nil {
-			respondwithJSON(w, http.StatusInternalServerError, fmt.Sprintf("error found: %s", err.Error()))
-		}
-	*/
-	respondwithJSON(w, http.StatusOK, apiPokemon)
+	return apiPokemon, nil
 }
--- curso-go-unit-testing-base-initial-project/go.mod
+++ curso-go-unit-testing-base-initial-project/go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/gorilla/mux v1.8.0
-	github.com/stretchr/testify v1.8.0
+	github.com/jarcoal/httpmock v1.2.0
+	github.com/stretchr/testify v1.8.1
 )
