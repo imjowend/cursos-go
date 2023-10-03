4fd9d47e72dc28412c1943657638cb30001bb1f5
jowend <joaquinvcestudio@gmail.com>
2023-10-03 05:07:22 -0300

    Excercise 3 Done!

--- hacker-rank/3-months-preparation-kit/1-week/3-time-conversion/time-conversion.go
+++ hacker-rank/3-months-preparation-kit/1-week/3-time-conversion/time-conversion.go
@@ -7,66 +7,32 @@ import (
 )
 
 func timeConversion(s string) string {
-	// Write your code here
-	fmt.Println(s)
-	fmt.Println("s 0: ", s[0])
-	fmt.Println("s 1: ", s[1])
 	var hour24 string
 	if strings.Contains(s, "PM") {
-		if !(s[0] == 1 && s[1] == 2) {
+		if !(s[0:2] == "12") {
 			hour := strings.TrimSpace(s[0:2])
-			fmt.Println("hour: ", hour)
 			intHour, err := strconv.Atoi(hour)
-			fmt.Println("intHour: ", intHour)
 			if err != nil {
 				return ""
 			}
 			intHour += 12
 			newHour := strconv.Itoa(intHour)
-			fmt.Println("newHour: ", newHour)
 			hourChanged := strings.Replace(s, hour, newHour, 1)
-			fmt.Println("hourChanged: ", hourChanged)
 			militaryHour := strings.Trim(hourChanged, "PM")
-			fmt.Println("militaryHour", militaryHour)
 			hour24 = militaryHour
-			fmt.Println("hour24:", hour24)
-
 		} else {
 			militaryHour := strings.Trim(s, "PM")
-			fmt.Println("militaryHour", militaryHour)
-
 			hour24 = militaryHour
-			fmt.Println("hour24:", hour24)
-
 		}
 	} else if strings.Contains(s, "AM") {
-		if s[0] == 1 && s[1] == 2 {
+		if s[0:2] == "12" {
 			hour := strings.TrimSpace(s[0:2])
-			fmt.Println("hour: ", hour)
-
-			intHour, err := strconv.Atoi(hour)
-			fmt.Println("intHour: ", intHour)
-
-			if err != nil {
-				return ""
-			}
-			intHour -= 12
-			newHour := strconv.Itoa(intHour)
-			fmt.Println("newHour: ", newHour)
-
-			hourChanged := strings.Replace(s, hour, newHour, 1)
-			fmt.Println("hourChanged: ", hourChanged)
-
+			hourChanged := strings.Replace(s, hour, "00", 1)
 			militaryHour := strings.Trim(hourChanged, "AM")
-			fmt.Println(militaryHour)
 			hour24 = militaryHour
-			fmt.Println("hour24", hour24)
-
 		} else {
 			militaryHour := strings.Trim(s, "AM")
 			hour24 = militaryHour
-			fmt.Println("hour24", hour24)
-
 		}
 	}
 	return hour24
@@ -74,7 +40,7 @@ func timeConversion(s string) string {
 
 func main() {
 
-	result := timeConversion("12:01:01AM")
+	result := timeConversion("07:01:01AM")
 
 	fmt.Println(result)
 
