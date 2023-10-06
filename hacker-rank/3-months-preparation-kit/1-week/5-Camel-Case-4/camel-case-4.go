f5e963b3a68a0df125ee340012d90ad983116e48
jowend <joaquinvcestudio@gmail.com>
2023-10-06 19:41:26 -0300

    test parts

--- hacker-rank/3-months-preparation-kit/1-week/5-Camel-Case-4/camel-case-4.go
+++ hacker-rank/3-months-preparation-kit/1-week/5-Camel-Case-4/camel-case-4.go
@@ -1,11 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	//Enter your code here. Read input from STDIN. Print output to STDOUT
-	var word string
-	fmt.Print("Type a word: ")
+	var tests []string
+
+	tests = append(tests, "S;M;plasticCup()", "C;V;mobile phone", "C;C;coffee machine", "S;C;LargeSoftwareBook", "C;M;white sheet of paper", "S;V;pictureFrame")
+	fmt.Println(tests)
+
+	for i, test := range tests {
+
+		fmt.Println("Test N°"+strconv.Itoa(i)+" \n word: ", test)
+		partsOfTest := strings.Split(test, ";")
+		for i, part := range partsOfTest {
+			//The first part must contains (S or C)
+			if part == "S" {
+
+			} else {
+
+			}
+			//
+			fmt.Println("Part N°"+strconv.Itoa(i)+"\n part: ", part)
+		}
+
+	}
+	/*fmt.Print("Type a word: ")
 	fmt.Scan(&word)
-	fmt.Println("Your word is:", word)
+	fmt.Println("Your word is:", word)*/
 }
