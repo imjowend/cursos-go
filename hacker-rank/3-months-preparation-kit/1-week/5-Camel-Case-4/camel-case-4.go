1e7a44e4b74f98060a8f3d152d3be27b153c148d
jowend <joaquinvcestudio@gmail.com>
2023-10-05 22:25:26 -0300

    camel case 4

---
+++ hacker-rank/3-months-preparation-kit/1-week/5-Camel-Case-4/camel-case-4.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Enter your code here. Read input from STDIN. Print output to STDOUT
+	var word string
+	fmt.Print("Type a word: ")
+	fmt.Scan(&word)
+	fmt.Println("Your word is:", word)
+}
