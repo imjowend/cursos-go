d4cc06bbb81c372d677f5e0e5f4e31c2fbc2b9a5
jowend <joaquinvcestudio@gmail.com>
2024-01-03 19:38:32 -0500

    sparse arrays

---
+++ hacker-rank/3-months-preparation-kit/1-week/7-sparse-arrays/sparse-arrays.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var inputString = []string{"ab", "ab", "abc"}
+	var queryString = []string{"ab", "abc", "bc"}
+
+	fmt.Println(matchingStrings(inputString, queryString))
+}
+
+func matchingStrings(inputString []string, queryString []string) []int32 {
+	// Write your code here
+	var res []int32
+
+	for i := 0; i < len(inputString); i++ {
+		var count int32
+		for j := 0; j < len(queryString); j++ {
+
+			fmt.Println("inputString: ", inputString[j])
+			fmt.Println("queryString", queryString[i])
+			if inputString[j] == queryString[i] {
+				fmt.Println("+1")
+				count++
+			}
+		}
+		res = append(res, count)
+	}
+
+	return res
+}
