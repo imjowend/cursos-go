8b5e315dacb958ae9e638afe65ea7ce197e885cd
jowend <joaquinvcestudio@gmail.com>
2024-01-03 19:42:14 -0500

    sparse arrays fix

--- hacker-rank/3-months-preparation-kit/1-week/7-sparse-arrays/sparse-arrays.go
+++ hacker-rank/3-months-preparation-kit/1-week/7-sparse-arrays/sparse-arrays.go
@@ -15,9 +15,9 @@ func matchingStrings(inputString []string, queryString []string) []int32 {
 	// Write your code here
 	var res []int32
 
-	for i := 0; i < len(inputString); i++ {
+	for i := 0; i < len(queryString); i++ {
 		var count int32
-		for j := 0; j < len(queryString); j++ {
+		for j := 0; j < len(inputString); j++ {
 
 			fmt.Println("inputString: ", inputString[j])
 			fmt.Println("queryString", queryString[i])
