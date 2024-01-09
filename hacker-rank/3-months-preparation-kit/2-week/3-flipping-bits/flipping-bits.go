6e19b2745c9e389374d6dbedb2665ad0a3567600
jowend <joaquinvcestudio@gmail.com>
2024-01-09 16:11:33 -0500

    flipping bits

--- hacker-rank/3-months-preparation-kit/2-week/3-flipping-bits/flipping-bits.go
+++ hacker-rank/3-months-preparation-kit/2-week/3-flipping-bits/flipping-bits.go
@@ -2,27 +2,49 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
 const (
-	num = 2147483647
+	num       = 35601423
+	maxLength = 32
+	one       = "1"
+	zero      = "0"
 )
 
 func main() {
-	number := 10
-	fmt.Println(flippingBits(int64(number)))
+	fmt.Println(flippingBits(int64(num)))
 }
 
 func flippingBits(n int64) int64 {
-	// Write your code here
-	//cacmbiar a decimal
-
+	var invertedBinNum string
 	newInt := strconv.FormatInt(n, 2)
-	b, _ := strconv.Atoi(newInt)
-	newBinNum := fmt.Sprintf("%032d", int64(b))
-	fmt.Println(newBinNum)
-	a, _ := strconv.Atoi(newBinNum)
+	newBinNum := completeZeros(newInt)
+	for i := 0; i < len(newBinNum); i++ {
+		if newBinNum[i] == '0' {
+			invertedBinNum += one
+		}
+		if newBinNum[i] == '1' {
+			invertedBinNum += zero
+		}
+	}
+	var entero int64
 
-	return int64(a)
+	for i, j := 0, len(invertedBinNum)-1; i <= len(invertedBinNum)-1; i, j = i+1, j-1 {
+		if invertedBinNum[i] == '1' {
+			entero += int64(math.Pow(2, float64(j)))
+		}
+	}
+	return entero
+}
+
+func completeZeros(str string) string {
+	lengthOfZeros := maxLength - len(str)
+	var newstr string
+	for i := 0; i < lengthOfZeros; i++ {
+		newstr += zero
+	}
+
+	return newstr + str
 }
