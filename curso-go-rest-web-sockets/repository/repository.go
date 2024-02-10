4fc9ebf7cd557724009977704dfdde2450685c7e
jowend <joaquinvcestudio@gmail.com>
2024-02-09 19:42:55 -0500

    Post repository

--- curso-go-rest-web-sockets/repository/repository.go
+++ curso-go-rest-web-sockets/repository/repository.go
@@ -6,16 +6,17 @@ import (
 	"github.com/imjowend/cursos-go/curso-go-rest-web-sockets/models"
 )
 
-type UserRepository interface {
+type Repository interface {
 	InsertUser(ctx context.Context, user *models.User) error
 	GetUserById(ctx context.Context, id string) (*models.User, error)
 	GetUserByEmail(ctx context.Context, email string) (*models.User, error)
 	Close() error
+	InsertPost(ctx context.Context, post *models.Post) error
 }
 
-var implementation UserRepository
+var implementation Repository
 
-func SetRepository(repository UserRepository) {
+func SetRepository(repository Repository) {
 	implementation = repository
 }
 
@@ -33,4 +34,8 @@ func Close() error {
 
 func GetUserByEmail(ctx context.Context, id string) (*models.User, error) {
 	return implementation.GetUserByEmail(ctx, id)
+}
+
+func InsertPost(ctx context.Context, post *models.Post) error {
+	return implementation.InsertPost(ctx, post)
 }
