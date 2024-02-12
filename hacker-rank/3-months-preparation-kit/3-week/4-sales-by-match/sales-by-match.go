21bc458591aa056ed727c3ffb44452a993121e2b
jowend <joaquinvcestudio@gmail.com>
2024-02-12 15:38:02 -0500

    done

--- hacker-rank/3-months-preparation-kit/3-week/4-sales-by-match/sales-by-match.go
+++ hacker-rank/3-months-preparation-kit/3-week/4-sales-by-match/sales-by-match.go
@@ -17,38 +17,56 @@ func main() {
 
 }
 
+// func sockMerchant(n int32, ar []int32) int32 {
+// 	// Write your code here
+// 	var repeats []int32
+
+// 	numOfPairs := 0
+// 	for i := 0; i < int(n)-1; i++ {
+// 		isRepeat := false
+// 		if i >= 1 && len(repeats) >= 1 {
+// 			for j := 0; j < len(repeats); j++ {
+// 				if ar[i] == repeats[j] {
+// 					isRepeat = true
+// 					break
+// 				}
+// 			}
+// 		}
+// 		if isRepeat {
+// 			continue
+// 		}
+// 		counter := 1
+// 		for k := i + 1; k < int(n)-1; k++ {
+// 			if ar[i] == ar[k] {
+// 				counter++
+// 			}
+// 		}
+// 		pairs := counter / 2
+// 		numOfPairs += pairs
+// 		if numOfPairs == int(n)/2 {
+// 			break
+// 		} else {
+// 			repeats = append(repeats, ar[i])
+// 		}
+// 	}
+
+// 	return int32(numOfPairs)
+// }
+
 func sockMerchant(n int32, ar []int32) int32 {
 	// Write your code here
-	var repeats []int32
+	fmt.Println(ar)
+	pairs := make(map[int32]int32)
+	var numPairs int32
 
-	numOfPairs := 0
-	for i := 0; i < int(n)-1; i++ {
-		isRepeat := false
-		if i >= 1 && len(repeats) >= 1 {
-			for j := 0; j < len(repeats); j++ {
-				if ar[i] == repeats[j] {
-					isRepeat = true
-					break
-				}
-			}
-		}
-		if isRepeat {
-			continue
-		}
-		counter := 1
-		for k := i + 1; k < int(n)-1; k++ {
-			if ar[i] == ar[k] {
-				counter++
-			}
-		}
-		pairs := counter / 2
-		numOfPairs += pairs
-		if numOfPairs == int(n)/2 {
-			break
-		} else {
-			repeats = append(repeats, ar[i])
-		}
+	for _, i := range ar {
+		fmt.Println(i)
+		pairs[i]++
+	}
+	fmt.Println(pairs)
+	for _, v := range pairs {
+		numPairs += v / 2
 	}
 
-	return int32(numOfPairs)
+	return numPairs
 }
